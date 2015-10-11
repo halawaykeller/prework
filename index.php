c2f3f84b387e8d3c8d048acd491ea656ede00e8f
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-10 20:03:04 -0600

    edited index.php to reflect social media example in class text

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-	<html>
-	<head>
-		<title>I Completed the Prework</title>
-	</head>
-	<body>
-		<p>I completed the prework.</p>
-		<p><b>YAY!</b></p>
-	</body>
+<?php // Example 26-4: index.php
+require_once 'header.php';
+
+echo "<br><span class='main'>Welcome to $appname,";
+
+if ($loggedin) echo " $user, you are logged in.";
+else           echo ' please sign up and/or log in to join in.';
+?>
+
+</span><br><br>
+</body>
 </html>
